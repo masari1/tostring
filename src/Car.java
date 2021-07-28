d1dee97ea585a09616cba695ebe4fc998fcd3403
Malik Sari <Malik.Sari@outlook.com>
2021-07-28 17:28:01 +0200

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+
+
+    String make = "Ford";
+    String model = "mustang";
+    String colour= "red";
+    int year = 2021;
+
+
+    public String toString(){
+
+
+        return make + "\n" + model + "\n" + colour + "\n" + year;
+    }
+
+
+
+}
